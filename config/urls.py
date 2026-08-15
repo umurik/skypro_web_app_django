8474a515543eb193abb35b9cbfae59dd99266043
umurik <uuldasev003@gmail.com>
2026-08-15 14:32:35 +0700

    fixing urls

--- config/urls.py
+++ config/urls.py
@@ -16,13 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-
-from catalog.views import contacts, home
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", home),
-    path("home/", home),
-    path("contacts/", contacts),
+    path("", include("catalog.urls")),
 ]
